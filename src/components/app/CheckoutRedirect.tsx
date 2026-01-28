624773b3d29f86026254c71d8ae9b9972bf4a244
dahrabounizar <dahrabounizar@gmail.com>
2026-01-28 22:10:48 +0100

    v2

--- src/components/app/CheckoutRedirect.tsx
+++ src/components/app/CheckoutRedirect.tsx
@@ -7,7 +7,7 @@
 
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { supabase } from '../lib/supabase';
+import { supabase } from '../../lib/supabase';
 import { Loader2 } from 'lucide-react';
 
 export const CheckoutRedirect: React.FC = () => {
